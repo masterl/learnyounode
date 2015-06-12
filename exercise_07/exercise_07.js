d5060c21f6e4f1e3c9b8a78e7cffc2f5337f98e4
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2015-06-11 22:56:33 -0300

    Refactored 07 using methods suggested

--- exercise_07/exercise_07.js
+++ exercise_07/exercise_07.js
@@ -26,10 +26,6 @@ var url = process.argv[2]
 
 http.get(url, function(response) {
     response.setEncoding('utf8')
-    response.on('data',function(data){
-        console.log(data)
-    })
-    response.on('error',function(msg){
-        console.error(msg)
-    })
+    response.on('data',console.log)
+    response.on('error',console.error)
 })
